b70bcd3980025f272ba9d89c8e6ecd5b47d40337
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-12-20 15:14:13 -0700

    Fix Build errors

--- lib/import-monster/parseDefences.tsx
+++ lib/import-monster/parseDefences.tsx
@@ -1,4 +1,4 @@
-export default function parseDefences(input: string, text: string) {
+export default function parseDefences(input: string, text: string): string[] {
   const regExp: RegExp = new RegExp(`(?<=\\n)${text}.+(?=\\n)`);
 
   const match = input.match(regExp);
@@ -9,5 +9,5 @@ export default function parseDefences(input: string, text: string) {
 
   // Get all the words. The first two words will not be in the array (Ex: "Damage Vulnerabilities")
   const res = words.map((w, i) => i > 1 && w.toLowerCase()).filter(w => typeof w === "string");
-  return res;
+  return res as string[];
 }
