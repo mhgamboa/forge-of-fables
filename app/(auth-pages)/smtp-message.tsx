b6dfb284f4355eee0165b72c2723f09daaa5054b
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-12-20 09:00:16 -0700

    Center SMTP Message with mx-auto

--- app/(auth-pages)/smtp-message.tsx
+++ app/(auth-pages)/smtp-message.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export function SmtpMessage() {
   return (
-    <div className="bg-muted/50 px-5 py-3 border rounded-md flex gap-4">
+    <div className="bg-muted/50 px-5 py-3 border rounded-md flex gap-4 mx-auto">
       <InfoIcon size={16} className="mt-0.5" />
       <div className="flex flex-col gap-1">
         <small className="text-sm text-secondary-foreground">
-          <strong> Note:</strong> Emails are rate limited. Enable Custom SMTP to
-          increase the rate limit.
+          <strong> Note:</strong> Emails are rate limited. Enable Custom SMTP to increase the rate
+          limit.
         </small>
         <div>
           <Link
