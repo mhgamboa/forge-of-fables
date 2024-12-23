7b1050305cd3f5cc1f3bc88f440bfa50887de00d
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-12-23 12:40:15 -0800

    Remove All access to encounter_json

--- types/database.types.ts
+++ types/database.types.ts
@@ -9,33 +9,6 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      encounter_jsons: {
-        Row: {
-          created_at: string
-          encounter_json: Json
-          id: number
-          name: string
-          updated_at: string
-          user_id: string
-        }
-        Insert: {
-          created_at?: string
-          encounter_json?: Json
-          id?: number
-          name: string
-          updated_at?: string
-          user_id: string
-        }
-        Update: {
-          created_at?: string
-          encounter_json?: Json
-          id?: number
-          name?: string
-          updated_at?: string
-          user_id?: string
-        }
-        Relationships: []
-      }
       encounter_monsters: {
         Row: {
           created_at: string
